4e695cc0bc828e432f50433cf70f055e4894f2ec
LI CI (Lilian) CHUANG <65158230+lilianchi@users.noreply.github.com>
2022-02-13 15:08:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,8 @@ st.markdown("""
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#li-ci-(lilian)-chuang">Home</a>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="/">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#education">Education</a>
