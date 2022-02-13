dd16fe961d54d675d27327c5860bd9dbff763aa0
LI CI (Lilian) CHUANG <65158230+lilianchi@users.noreply.github.com>
2022-02-13 14:57:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ st.markdown("""
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link disabled" href="/">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#home">Home</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#education">Education</a>
