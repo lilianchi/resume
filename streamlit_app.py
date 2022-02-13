8dbb35b0f5983fb03f2313f94f051fc3a811871d
LI CI (Lilian) CHUANG <65158230+lilianchi@users.noreply.github.com>
2022-02-13 10:03:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,7 +95,7 @@ def txt4(a, b, c):
     st.markdown(c)
 
 def txt5(a, b, c, d):
-  col1, col2, col3, col4 = st.columns([0.8,2.5,4.5,2])
+  col1, col2, col3, col4 = st.columns([1,2.5,4.5,2])
   with col1:
     st.markdown(f'`{a}`')
   with col2:
