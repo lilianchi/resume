7e4b400ba784edfe9fc884657fbeb9b71bd0be22
LI CI (Lilian) CHUANG <65158230+lilianchi@users.noreply.github.com>
2022-02-13 15:13:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ with open("style.css") as f:
 #####################
 # Header 
 st.write('''
-# Li-Ci (Lilian) Chuang
+# Li-Ci(Lilian) Chuang
 ##### *Resume* 
 ''')
 
@@ -45,7 +45,7 @@ st.markdown("""
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#li-ci(lilian)-chuang">Education</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#education">Education</a>
